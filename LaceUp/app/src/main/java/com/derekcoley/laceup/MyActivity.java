a17db07ae55dcbefd6510fc1d6e3e23eec51a3ab
dmcoley <dmcoley@uw.edu>
2016-03-19 07:43:50 -0700

    merge please

--- LaceUp/app/src/main/java/com/derekcoley/laceup/MyActivity.java
+++ LaceUp/app/src/main/java/com/derekcoley/laceup/MyActivity.java
@@ -1,20 +1,15 @@
 package com.derekcoley.laceup;
 
 import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Base64;
-import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
@@ -24,8 +19,6 @@ import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
 
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 
 public class MyActivity extends AppCompatActivity {
     public final static String EXTRA_MESSAGE = "com.mycompany.myfirstapp.MESSAGE";

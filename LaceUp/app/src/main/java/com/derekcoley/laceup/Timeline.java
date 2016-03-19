5a7feeac770632e929abad1b24f10e479521d6f9
dmcoley <dmcoley@uw.edu>
2016-03-19 02:21:02 -0700

    fixing Bryants bugs...

--- LaceUp/app/src/main/java/com/derekcoley/laceup/Timeline.java
+++ LaceUp/app/src/main/java/com/derekcoley/laceup/Timeline.java
@@ -7,9 +7,6 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 
 public class Timeline extends AppCompatActivity {
     public final static String EXTRA_MESSAGE = "com.mycompany.myfirstapp.MESSAGE";

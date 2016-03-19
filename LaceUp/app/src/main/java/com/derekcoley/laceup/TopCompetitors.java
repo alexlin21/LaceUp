a0b0d1f38ea59949c18bd23133d2395dd9c632f6
dmcoley <dmcoley@uw.edu>
2016-03-19 03:02:14 -0700

    animations

--- LaceUp/app/src/main/java/com/derekcoley/laceup/TopCompetitors.java
+++ LaceUp/app/src/main/java/com/derekcoley/laceup/TopCompetitors.java
@@ -1,15 +1,7 @@
 package com.derekcoley.laceup;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.app.Activity;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 
 public class TopCompetitors extends AppCompatActivity {
 

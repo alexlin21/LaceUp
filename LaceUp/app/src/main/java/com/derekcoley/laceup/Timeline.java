157801c829726b1c29dc3c0da61a70b8a320d8d4
dmcoley <dmcoley@uw.edu>
2016-03-18 23:29:24 -0700

    timeline

--- LaceUp/app/src/main/java/com/derekcoley/laceup/Timeline.java
+++ LaceUp/app/src/main/java/com/derekcoley/laceup/Timeline.java
@@ -1,6 +1,5 @@
 package com.derekcoley.laceup;
 
-import android.widget.RelativeLayout;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,9 +7,10 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
-public class DisplayMessageActivity extends AppCompatActivity {
+public class Timeline extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,4 +39,5 @@ public class DisplayMessageActivity extends AppCompatActivity {
         RelativeLayout layout = (RelativeLayout) findViewById(R.id.content);
         layout.addView(textView);
     }
+
 }

f7e5b521213f008e660bc1f638209301696bbbed
dmcoley <dmcoley@uw.edu>
2016-03-21 17:08:11 -0700

    refactor Challenges

--- LaceUp/app/src/main/java/com/derekcoley/laceup/Challenges.java
+++ LaceUp/app/src/main/java/com/derekcoley/laceup/Challenges.java
@@ -1,11 +1,36 @@
 package com.derekcoley.laceup;
 
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MotionEvent;
-import android.widget.ViewFlipper;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class Challenges extends AppCompatActivity {
+public class Challenges extends BaseMenu {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_challenges2);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    /*
     private ViewFlipper viewFlipper;
     private float lastX;
 
@@ -53,4 +78,5 @@ public class Challenges extends AppCompatActivity {
         }
         return false;
     }
+    */
 }
